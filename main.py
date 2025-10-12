d10d2c73d18e65df9e46b04a230f3d0cfc753940
thebladehit <boqdanyarmolka@gmail.com>
2025-10-12 15:15:05 +0300

    Add drawing logic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
+
 neighborhood_coords = [[0, -1], [1, 0], [0, 1], [-1, 0]]
 
+# Core logic
 def get_vertex_neighborhood(x: int, y: int, maxX: int, maxY: int):
     neighborhood_vertices_coords = []
     for dx, dy in neighborhood_coords:
@@ -20,5 +23,35 @@ def generate_grid_graph(rowCount: int, colCount: int):
             graph[(x, y)] = get_vertex_neighborhood(x, y, colCount, rowCount)
     return graph
 
+# Draw logic
+def draw_edges(graph: dict):
+    drawn_edges = set()
+    for vertex, neighbors in graph.items():
+        x1, y1 = vertex
+        for neighbor in neighbors:
+            edge = tuple(sorted([vertex, neighbor]))
+            if edge not in drawn_edges:
+                x2, y2 = neighbor
+                plt.plot([x1, x2], [y1, y2], color="gray", linewidth=2)
+                drawn_edges.add(edge)
+
+def draw_vertices(graph: dict):
+    for vertex in graph.keys():
+        x, y = vertex
+        plt.scatter(x, y, color="skyblue", s=150, edgecolors="black", zorder=3)
+        plt.text(x, y + 0.2, f"{vertex}", ha="center", fontsize=8)
+
+def draw_graph(graph: dict, rowSize: int, colSize: int):
+    plt.figure(figsize=(colSize, rowSize))
+    draw_edges(graph)
+    draw_vertices(graph)
+    plt.axis("equal")
+    plt.gca().invert_yaxis()
+    plt.axis("off")
+    plt.title("Візуалізація графа (дороги)")
+    plt.show()
+
 graph = generate_grid_graph(5, 5)
 print(graph)
+
+draw_graph(graph, 5, 5)
